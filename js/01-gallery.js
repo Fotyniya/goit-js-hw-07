fa101468abdea285b3590ba19c9318834d856906
Fotyniya <s.kozachenko@ekwo.dp.ua>
2022-12-04 15:34:44 +0200

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const imgContainer = document.querySelector ('.gallery');
+const imagesMarkup = createImgCardsMarkup(galleryItems);
+
+imgContainer.insertAdjacentHTML('beforeend', imagesMarkup);
+
+imgContainer.addEventListener("click", (event) => {
+    event.preventDefault();
+    
+    if (!event.target.classList.contains('gallery__image')){
+        return;
+    }
+    
+    const instance = basicLightbox.create(
+        event.target.closest('.gallery__link').innerHTML, {
+        onShow: instance => {
+            document.addEventListener('keydown', onEscKeyClick);
+            },
+        onClose: instance => {
+            document.removeEventListener('keydown', onEscKeyClick);
+            },
+        }
+    );
+    
+    instance.show();
+    
+    function onEscKeyClick(event) {
+        if (event.code !== 'Escape') {
+            return;
+        } 
+        instance.close();   
+    }
+});
+  
+function createImgCardsMarkup (galleryItems) {
+    return galleryItems
+    .map(({preview, original, description}) => {
+    return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class = "gallery__image"
+      src = "${preview}"
+      data-source ="${original}"
+      alt = "${description}"
+    />
+    </a>
+    </div>
+    `;
+    }).join('')
+}
